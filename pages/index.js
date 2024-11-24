f6595164bc8a775aeabe24532c98066e71625d79
Matteo Librizzi <mttlbr@amazon.lu>
2024-11-24 18:27:41 +0100

    Added form B link

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ import { useABTestPool } from "../hooks/useABTestPool";
 import { useEffect } from "react";
 
 const formALink =
-  "https://docs.google.com/forms/d/e/1FAIpQLSdHYPyfGS7iXfXr1Qb1ovd03vbPDslxPajOavZI5TJlXGu2qA/viewform";
-const formBLink = "";
+  "https://docs.google.com/forms/d/e/1FAIpQLSdHYPyfGS7iXfXr1Qb1ovd03vbPDslxPajOavZI5TJlXGu2qA/viewform?usp=sf_link";
+const formBLink =
+  "https://docs.google.com/forms/d/e/1FAIpQLSf_11QBY6cJu9OQ4ChcrAlaBWQwEYBy6jGGBdF_1SaWCXujqA/viewform?usp=sf_link";
 
 export default function Home() {
   const isPoolA = useABTestPool();
